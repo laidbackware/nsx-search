7a4a4daf892ad1f18edff399c0bbd20458c3dae3
Matt Proud <proudmatt@gmail.com>
2020-12-14 09:21:58 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 VERSION=$1
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-env GOOS=linux GOARCH=amd64 go build -o ${SCRIPT_DIR}/builds/nsxsearch-linux-VERSION
+env GOOS=linux GOARCH=amd64 go build -o ${SCRIPT_DIR}/builds/nsxsearch-linux-${VERSION}
 
-env GOOS=windows GOARCH=amd64 go build -o ${SCRIPT_DIR}/builds/nsxsearch-windows-VERSION
+env GOOS=windows GOARCH=amd64 go build -o ${SCRIPT_DIR}/builds/nsxsearch-windows-${VERSION}
 
-env GOOS=darwin GOARCH=amd64 go build -o ${SCRIPT_DIR}/builds/nsxsearch-darwin-VERSION
+env GOOS=darwin GOARCH=amd64 go build -o ${SCRIPT_DIR}/builds/nsxsearch-darwin-${VERSION}
